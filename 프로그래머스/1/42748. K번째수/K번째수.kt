e3a37128ecd65677a6bdd289629e130cd52e9974
dlho1222 <127379797+dlho1222@users.noreply.github.com>
2023-11-07 15:42:52 +0900

    [level 1] Title: K번째수, Time: 13.98 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.kt
@@ -0,0 +1,9 @@
+class Solution {
+        fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+            var answer = intArrayOf()
+            for(i in commands.indices){
+                answer += array.copyOfRange(commands[i][0]-1,commands[i][1]).sorted()[commands[i][2]-1]
+            }
+            return answer
+        }
+    }
