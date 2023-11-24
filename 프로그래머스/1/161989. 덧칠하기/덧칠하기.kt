676a7ec46ebe8762e8a855a4fc752bee3afd0962
dlho1222 <127379797+dlho1222@users.noreply.github.com>
2023-11-24 09:58:32 +0900

    [level 1] Title: 덧칠하기, Time: 1.16 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
@@ -0,0 +1,15 @@
+class Solution {
+        fun solution(n: Int, m: Int, section: IntArray): Int {
+            var answer: Int = 0
+            var endPoint = 0
+            answer = section.fold(0){count,area->
+                if(endPoint < area){
+                    endPoint = area + m - 1
+                    count+ 1
+                }else{
+                    count
+                }
+            }
+            return answer
+        }
+    }
