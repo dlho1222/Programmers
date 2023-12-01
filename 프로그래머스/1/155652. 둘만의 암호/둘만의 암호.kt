d3660d549caa51a819eda6f3a2b1a2c4eb93c938
dlho1222 <127379797+dlho1222@users.noreply.github.com>
2023-12-01 09:09:00 +0900

    [level 1] Title: 둘만의 암호, Time: 10.31 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
@@ -1,13 +1,12 @@
-class Solution {
+ class Solution {
         fun solution(s: String, skip: String, index: Int): String {
             var answer: String = ""
-            var skipChar = skip.toCharArray().map { it }
-            var skippedChar = ('a'..'z').filter { it !in skipChar } 
+            var skippedChar = ('a'..'z').filter { it !in skip }
+
             s.forEach {
-                var newIndex = (skippedChar.indexOf(it)+index ) % skippedChar.size 
+                var newIndex = (skippedChar.indexOf(it) + index) % skippedChar.size
                 answer += skippedChar[newIndex]
             }
-            println(answer)
             return answer
         }
     }
