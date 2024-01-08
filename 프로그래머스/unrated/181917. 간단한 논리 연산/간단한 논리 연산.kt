d0a24b30b584c09794d6caf9be77c0a06b636985
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-01-08 09:26:26 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.kt
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.kt
@@ -1,8 +1,6 @@
 class Solution {
-        fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
-            var answer: Boolean = false
-            if ((x1 || x2) && (x3 || x4)) answer = true
-                print(answer)
-            return answer
+        fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean{ 
+           return (x1 || x2) && (x3 || x4)
+              
         }
     }
