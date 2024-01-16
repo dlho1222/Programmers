2049d8738e9fb8340b62daecc4ed34e65cf22982
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-01-16 09:45:01 +0900

    [level Lv.0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
+++ 프로그래머스/Lv.0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
@@ -1,12 +1,7 @@
 class Solution {
         fun solution(num_list: IntArray): Int {
             var answer: Int = -1
-            for (i in num_list.indices) {
-                if (0 > num_list[i]) {
-                    answer = i
-                    break
-                }
-            }
+            answer = num_list.indexOfFirst { it < 0 }
             return answer
         }
     }
