897b372438e91d46027ba80617f65c1886a79063
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-01-18 10:01:11 +0900

    [level 0] Title: 할 일 목록, Time: 9.29 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
@@ -0,0 +1,9 @@
+class Solution {
+        fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String> {
+            var answer: Array<String> = arrayOf<String>()
+            for (i in todo_list.indices) {
+                if (finished[i]) continue else answer += todo_list[i]
+            }
+            return answer
+        }
+    }
