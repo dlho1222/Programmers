e29abc91c4be04baf9a7a6cb8bc711d1477d5a75
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-01-18 09:51:11 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 5.75 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.kt
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.kt
@@ -1,9 +1,6 @@
 class Solution {
-        fun solution(num_list: IntArray): Int {
-            var even = 0
-            var odd = 0
-            even =num_list.filterIndexed { index, _ -> index % 2 == 0 }.sum()
-            odd = num_list.filterIndexed { index, _ -> index % 2 != 0 }.sum()
-            return maxOf(even,odd)
-        }
+        fun solution(num_list: IntArray) =
+            maxOf(num_list.filterIndexed { index, _ -> index % 2 == 0 }.sum(),
+                num_list.filterIndexed { index, _ -> index % 2 != 0 }.sum()
+            )
     }
