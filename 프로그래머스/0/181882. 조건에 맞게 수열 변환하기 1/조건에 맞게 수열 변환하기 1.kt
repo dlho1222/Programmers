c01e0d87105d79f77349a7dec19b2c32c58b236b
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-01-19 09:52:01 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 4.27 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.kt
@@ -0,0 +1,9 @@
+class Solution {
+        fun solution(arr: IntArray): IntArray {
+            for (i in arr.indices) {
+                if (arr[i] % 2 == 0 && arr[i] >= 50) arr[i] = arr[i] / 2
+                else if (arr[i] % 2 != 0 && arr[i] <= 50) arr[i] = arr[i] * 2
+            }
+            return arr
+        }
+    }
