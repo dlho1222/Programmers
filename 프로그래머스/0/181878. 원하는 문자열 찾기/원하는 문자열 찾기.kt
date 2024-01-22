80c42209cb18de3bff8a9011340f4e61b0383dc0
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-01-22 10:07:30 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 8.11 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.kt
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.kt
@@ -1,9 +1,3 @@
 class Solution {
-        fun solution(myString: String, pat: String): Int {
-            var answer: Int = 0
-            val newString = myString.toLowerCase()
-            val newPat = pat.toLowerCase()
-            if(newString.contains(newPat)) answer = 1 
-            return answer
-        }
+        fun solution(myString: String, pat: String) = if(pat.toLowerCase() in myString.toLowerCase()) 1 else 0
     }
