83045a99d35e3b018bc076225dcc769e94b45ee2
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-01-24 09:51:15 +0900

    [level 0] Title: x 사이의 개수, Time: 17.92 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.kt
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.kt
@@ -1,4 +1,4 @@
 class Solution {
         fun solution(myString: String): IntArray =
-            myString.split("x").map { it.length }.toTypedArray().toIntArray()
+            myString.split("x").map { it.length }.toIntArray()
     }
