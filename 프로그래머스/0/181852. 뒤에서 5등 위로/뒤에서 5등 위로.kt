b718508426d1f1d0b8a25018489e3e42297fabe9
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-01-30 09:42:30 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 21.98 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.kt
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.kt
@@ -1,4 +1,4 @@
 class Solution {
         fun solution(num_list: IntArray): IntArray =
-            num_list.sorted().slice(5..num_list.lastIndex).toIntArray()
+            num_list.sorted().drop(5).toIntArray()
     }
