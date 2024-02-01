d422f08887d15902f0ee9c06591b7d7484d6a456
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-02-01 09:35:43 +0900

    [level 0] Title: 꼬리 문자열, Time: 12.81 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.kt
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.kt
@@ -1,9 +1,4 @@
 class Solution {
-        fun solution(str_list: Array<String>, ex: String): String {
-            var answer: String = ""
-            for (i in str_list.indices) {
-                if (!str_list[i].contains(ex)) answer += str_list[i]
-            }
-            return answer
-        }
+        fun solution(str_list: Array<String>, ex: String) =
+            str_list.filter { ex !in it }.joinToString("")
     }
