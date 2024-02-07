4557286bbb60d307368ff0a2a4daf504c3386add
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-02-07 09:30:42 +0900

    [level 0] Title: 옹알이 (1), Time: 13.29 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.kt
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.kt
@@ -1,8 +1,6 @@
 class Solution {
         fun solution(babbling: Array<String>): Int {
-            var answer: Int = 0
             var babble = babbling.map { it.replace("aya"," ").replace("ye"," ").replace("woo", " ").replace("ma"," ")}
-            answer = babble.map { it.replace(" ","") == ""}.count { it }
-            return answer
+            return babble.map { it.replace(" ","") == ""}.count { it }
         }
     }
