584f7dca5bc529bc40594c04fc3b6c071ddfa019
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-02-19 09:14:00 +0900

    [level 0] Title: 약수 구하기, Time: 7.38 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.kt
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.kt
@@ -1,9 +1,3 @@
 class Solution {
-        fun solution(n: Int): IntArray {
-            var answer: IntArray = intArrayOf()
-            for (i in 1..n) {
-                if (n % i == 0) answer += i
-            }
-            return answer
-        }
+        fun solution(n: Int): IntArray = (1..n).filter { n % it == 0 }.toIntArray()
     }
