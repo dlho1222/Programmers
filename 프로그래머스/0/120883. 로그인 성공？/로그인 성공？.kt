1f440538bf3cec4fa97fa62223cc32d956fe43d2
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-04-11 15:32:43 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.kt
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.kt
@@ -1,7 +1,13 @@
 class Solution {
         fun solution(id_pw: Array<String>, db: Array<Array<String>>): String {
-            val result = db.find { it[0] == id_pw[0] } ?: return "fail"
-            if(result[1] == id_pw[1]) return "login"
-            return "wrong pw"
+            var answer: String = "fail"
+            val id = id_pw[0]
+            val pw = id_pw[1]
+            db.forEach {
+                if(id == it[0]) {
+                    answer = if(pw == it[1]) "login" else "wrong pw"
+                }
+            }
+            return answer
         }
     }
