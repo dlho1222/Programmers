727b349c51003c02f8b7884e1434a9a73fc581cf
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-05-01 12:38:30 +0900

    [level 0] Title: 외계어 사전, Time: 37.99 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.kt
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.kt
@@ -1,14 +1,14 @@
 class Solution {
         fun solution(spell: Array<String>, dic: Array<String>): Int {
-            var answer: Int = 2
-            val sortedSpell = spell.sorted().joinToString("")
-            val sortedDic = dic.map { it.split("").sorted().joinToString("") }
+            var result = 2
+            var sortedSpell = spell.sorted().joinToString("")
+            var sortedDic = dic.map { it.split("").sorted().joinToString("") }
             for (i in sortedDic.indices) {
-                if (sortedSpell == sortedDic[i]) {
-                    answer = 1
+                if (sortedDic.contains(sortedSpell)) {
+                    result = 1
                     break
                 }
             }
-            return answer
+            return result
         }
     }
