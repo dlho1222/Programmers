1e1d31dfcc64417c88e86906405be028e5621c66
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-05-05 18:04:55 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 8.41 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.kt
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.kt
@@ -1,5 +1,4 @@
 class Solution {
         fun solution(my_string: String): Int =
-            my_string.split("[A-z]".toRegex()).filter { it.isNotEmpty() }
-                .sumOf { it.toInt() }
+            my_string.split("[A-z]".toRegex()).filter { it.isNotEmpty() }.sumOf { it.toInt() }
     }
