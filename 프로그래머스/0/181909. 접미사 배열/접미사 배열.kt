52fe99734b06b7a39c29f9ee5705589f7b30f131
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-05-07 13:10:23 +0900

    [level 0] Title: 접미사 배열, Time: 15.24 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.kt
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.kt
@@ -1,9 +1,5 @@
 class Solution {
-        fun solution(my_string: String): Array<String> {
-            val answer: MutableList<String> = mutableListOf()
-            for (i in my_string.indices) {
-                answer.add(my_string.substring(i, my_string.length))
-            }
-            return answer.sorted().toTypedArray()
-        }
+        fun solution(my_string: String): Array<String> =
+            my_string.indices.map { my_string.substring(it, my_string.length) }.sorted()
+                .toTypedArray()
     }
