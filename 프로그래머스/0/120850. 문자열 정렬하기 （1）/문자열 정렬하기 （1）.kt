f18305ffd3796bfc51d6b1aae3a7f839d514f501
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-05-11 11:54:23 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 15.93 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.kt
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.kt
@@ -1,4 +1,3 @@
 class Solution {
-        fun solution(my_string: String): IntArray =
-            my_string.filter { it.isDigit() }.map { it.digitToInt() }.sorted().toIntArray()
+        fun solution(my_string: String): IntArray = my_string.filter { it.isDigit()}.map { it.digitToInt() }.sorted().toIntArray()
     }
