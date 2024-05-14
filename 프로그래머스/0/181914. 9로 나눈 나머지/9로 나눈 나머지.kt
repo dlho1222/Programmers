1683509a5a3a3fe52f5cac038bcd9fc372fd6b1d
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-05-14 12:02:05 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 7.79 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.kt
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.kt
@@ -1,10 +1,3 @@
 class Solution {
-        fun solution(number: String): Int {
-            var answer: Int = 0
-            number.forEach {
-                answer += it.digitToInt()
-            }
-        
-            return answer % 9
-        }
+        fun solution(number: String): Int = number.map { it.digitToInt() }.sum() % 9
     }
