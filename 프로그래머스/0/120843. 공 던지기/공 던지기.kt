a79d391d9ab7085175caddaa5fb49c957ec5f494
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-05-19 15:09:11 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.kt
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.kt
@@ -1,16 +1,3 @@
- class Solution {
-        fun solution(numbers: IntArray, k: Int): Int {
-            var answer = numbers
-            var count = 0
-            var result = 0
-            repeat(k) {
-                answer += numbers
-            }
-            for (i in answer.indices step 2) {
-                if (count == k) break
-                result = answer[i]
-                count++
-            }
-            return result
-        }
+class Solution {
+        fun solution(numbers: IntArray, k: Int): Int = numbers[2 * (k - 1) % numbers.size]
     }
