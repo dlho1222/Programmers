cde593a65b9146e52ddb06f6c56bf8348a9b9821
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-05 20:28:59 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.84 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer = 0
-
-        var stringList : List<String> = n.toString().toCharArray().map { it.toString() }
-        answer = stringList.map { it.toInt() }.sum()
-        
-        return answer
+        fun solution(n: Int): Int = n.toString().sumOf { it.digitToInt() }
     }
-}
