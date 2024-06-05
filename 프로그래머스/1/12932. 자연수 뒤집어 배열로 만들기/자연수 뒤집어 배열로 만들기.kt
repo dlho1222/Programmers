5e08ce2456b1d7703cece826974293b3db486ca9
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-05 20:31:41 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 6.71 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
@@ -1,3 +1,3 @@
 class Solution {
-        fun solution(n: Long): IntArray = n.toString().toList().map { it.toString().toInt() }.reversed().toIntArray()
+        fun solution(n: Long): IntArray = n.toString().map { it.digitToInt() }.reversed().toIntArray()
     }
