8d6de7866de54a07aa1ea57ae6b93cba6bc3dc4f
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-06 13:30:06 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
@@ -1,7 +1,3 @@
 class Solution {
-        fun solution(num: Int): String {
-            var answer = ""
-            answer = if(num % 2 ==0) "Even" else "Odd"
-            return answer
-        }
+        fun solution(num: Int): String = if (num % 2 == 0) "Even" else "Odd"
     }
