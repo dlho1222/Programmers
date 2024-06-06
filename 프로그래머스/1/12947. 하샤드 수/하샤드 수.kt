ebebffdf0eb520cccac936343894490d5b43fbff
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-06 13:40:10 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
@@ -1,14 +1,11 @@
- class Solution {
+class Solution {
         fun solution(x: Int): Boolean {
-            var answer = true
-            val digit =  x.toString().toCharArray().map { it.toString().toInt() }
-            val count = digit.size
-            var result = 0
-
-            for(i in 0 until count) {
-                result += digit[i]
-                answer = if(x%result == 0) true else false
+            var sum = 0
+            var num = x
+            while (num > 0) {
+                sum += num % 10
+                num /= 10
             }
-            return answer
+            return x % sum == 0
         }
     }
