26afd72d6cbd5fc498d4837cfc59f55093ce08ee
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-06 14:00:25 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 3.15 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.kt
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.kt
@@ -1,12 +1,3 @@
 class Solution {
-        fun solution(n: Int): Int {
-            var answer: Int = 0
-            val list = mutableListOf<Int>()
-            for (x in 2..1000000) {
-                if (n % x == 1) list.add(x)
-            }
-            answer = list.first()
-            
-            return answer
-        }
+        fun solution(n: Int): Int = (2 until n).find { n % it == 1 } ?: 0
     }
