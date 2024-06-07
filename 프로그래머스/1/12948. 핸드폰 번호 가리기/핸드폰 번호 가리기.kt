967a9baf23b1115a13303e6d5ac46d1efe4ac777
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-07 13:49:09 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 7.05 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,26 +1,7 @@
 class Solution {
-        fun solution(phone_number: String): String {
-            var answer = ""
-            when(phone_number.length){
-                20->answer = phone_number.replaceRange(0..15,"****************")
-                19->answer = phone_number.replaceRange(0..14,"***************")
-                18->answer = phone_number.replaceRange(0..13,"**************")
-                17->answer = phone_number.replaceRange(0..12,"*************")
-                16->answer = phone_number.replaceRange(0..11,"************")
-                15->answer = phone_number.replaceRange(0..10,"***********")
-                14->answer = phone_number.replaceRange(0..9,"**********")
-                13->answer = phone_number.replaceRange(0..8,"*********")
-                12->answer = phone_number.replaceRange(0..7,"********")
-                11->answer = phone_number.replaceRange(0..6,"*******")
-                10->answer = phone_number.replaceRange(0..5,"******")
-                9->answer = phone_number.replaceRange(0..4,"*****")
-                8->answer = phone_number.replaceRange(0..3,"****")
-                7->answer = phone_number.replaceRange(0..2,"***")
-                6->answer = phone_number.replaceRange(0..1,"**")
-                5->answer = phone_number.replaceRange(0..0,"*")
-                4->answer = phone_number
-            }
-            
-            return answer
-        }
+        fun solution(phone_number: String): String = phone_number.replaceRange(
+            0,
+            phone_number.length - 4,
+            "*".repeat(phone_number.length - 4)
+        )
     }
