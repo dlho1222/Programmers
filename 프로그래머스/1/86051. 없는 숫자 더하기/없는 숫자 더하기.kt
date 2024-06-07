95cf31a625b850ee7aae19b14705949f38047881
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-07 13:26:35 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 20.33 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
@@ -1,10 +1,3 @@
 class Solution {
-        fun solution(numbers: IntArray): Int {
-            var answer: Int = 0
-                for(i in 0..9){
-                    if(numbers.contains(i) == false) answer+= i
-                }
-            
-            return answer
-        }
+        fun solution(numbers: IntArray): Int = (0..9).filter { it !in numbers }.sum()
     }
