6c2b1d850157e4e6fc9c96aa265d8176e671ada0
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-07 13:07:10 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 10.00 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -1,17 +1,3 @@
 class Solution {
-        fun solution(a: Int, b: Int): Long {
-            var answer: Long = 0
-
-            if( a < b) {
-                for (i in a..b) {
-                    answer += i
-                }
-            }else{
-                for (i in b..a) {
-                    answer += i
-                }
-            }
-           
-            return answer
-        }
+        fun solution(a: Int, b: Int): Long =(minOf(a,b).toLong()..maxOf(a,b).toLong()).toList().sum()
     }
