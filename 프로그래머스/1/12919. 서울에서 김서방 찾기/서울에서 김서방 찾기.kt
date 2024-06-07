98789d0d288c082b2a4e83e14a0e8a8dc50d8adc
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-07 13:09:31 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 24.39 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
@@ -1,8 +1,3 @@
 class Solution {
-        fun solution(seoul: Array<String>): String {
-            var answer = "김서방은 ${seoul.indexOfFirst { it.equals("Kim") }}에 있다"
-            
-            print(answer)
-            return answer
-        }
+        fun solution(seoul: Array<String>): String = "김서방은 ${seoul.indexOf("Kim")}에 있다"
     }
