0b41a32764731099399fe1f01c8e50a678f9490e
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-08 10:26:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
@@ -1,15 +1,11 @@
 class Solution {
         fun solution(num: Int): Int {
-            var answer = 0
-            var result = num.toLong()
-            if(num == 1) answer = 0
-            while (result != 1.toLong()){
-                answer++
-                result = if(result % 2 == 0.toLong()) result / 2 else (result * 3) + 1
-
+            var n = num.toLong()
+            var cnt = 0
+            while (n != 1.toLong()) {
+                if (n % 2 == 0.toLong()) n /= 2 else n = n * 3 + 1
+                cnt++
             }
-            if(answer > 500) answer = -1
-           
-            return answer
+            return if (cnt > 500) -1 else cnt
         }
     }
