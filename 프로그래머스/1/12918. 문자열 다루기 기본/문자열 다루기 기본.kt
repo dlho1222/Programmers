23b5adfb15b9877bef6d6c2393663700f1bc7bf3
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-10 12:54:52 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
@@ -1,10 +1,3 @@
 class Solution {
-        fun solution(s: String): Boolean {
-            var answer = false
-            if (s.length == 4 || s.length == 6) {
-                answer = s.all { it.isDigit() }
-            }
-           
-            return answer
-        }
+        fun solution(s: String): Boolean = if (s.length == 4 || s.length == 6) s.all { it.isDigit() } else false
     }
