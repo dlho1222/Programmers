d0ee8bedf3bd2674d9cfcb75c2254ede2f558a4d
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-11 12:05:10 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 13.10 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
@@ -1,7 +1,5 @@
 class Solution {
-        fun solution(s: String): String {
-            var answer = ""
-            answer = s.split(" ").joinToString(" "){word->word.mapIndexed { index, char -> if(index % 2 == 0) char.toUpperCase() else char.lowercase() }.joinToString("")}
-            return answer
-        }
-    }
+    fun solution(s: String): String = s.split(" ")
+                    .map { it.mapIndexed { index, c -> if (index % 2 == 0) c.uppercaseChar() else c.lowercaseChar() } }
+                    .joinToString(" ") { it.joinToString("") }
+}
