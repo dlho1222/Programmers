d6e2fea007316117cf38e5467575341c6fbe9302
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-11 11:13:37 +0900

    [level 1] Title: 직사각형 별찍기, Time: 255.29 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
@@ -1,9 +1,9 @@
- fun main(args: Array<String>) {
-        val (a, b) = readLine()!!.split(' ').map(String::toInt)
-        for(i in 0 until b){
-            for(j in 0 until a){
-                print("*")
-            }
-            println()
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+    for (i in 0 until b) {
+        for (j in 0 until a) {
+            print("*")
         }
+        println()
     }
+}
