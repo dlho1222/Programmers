7c85c96ee420160322f40aebae71fb388d40d709
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-12 11:55:47 +0900

    [level 1] Title: 최소직사각형, Time: 16.75 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -1,14 +1,10 @@
 class Solution {
         fun solution(sizes: Array<IntArray>): Int {
             var answer: Int = 0
-            var width = 0
             var height = 0
-            sizes.forEach {
-                width = maxOf(width,it[0], it[1])
-                height = maxOf(height,if (it[0] < it[1]) it[0] else it[1])
-            }
-           
-            answer = width * height
-            return answer
+            var width = 0
+            height = sizes.maxOfOrNull { maxOf(it[0], it[1]) }!!
+            width = sizes.maxOfOrNull { minOf(height, it[0], it[1]) }!!
+            return height * width
         }
     }
