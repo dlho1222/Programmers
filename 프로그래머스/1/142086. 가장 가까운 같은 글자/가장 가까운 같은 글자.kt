47ca4c7d8bd68b299bb516c8fd45d310c5c1ef2a
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-14 19:19:26 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.66 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.kt
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.kt
@@ -1,19 +1,15 @@
 class Solution {
         fun solution(s: String): IntArray {
-            var answer = IntArray(s.length, { -1 })
+            var answer = IntArray(s.length) { -1 }
             val charIndexMap = mutableMapOf<Char, Int>()
             for (i in s.indices) {
                 val char = s[i]
-                if (charIndexMap.containsKey(char)) {
+                if (charIndexMap.contains(char)) {
                     val lastIndex = charIndexMap[char]!!
-                    val distance = i - lastIndex
-                    if (answer[i] == -1 && answer[i] < distance) {
-                        answer[i] = distance
-                    }
+                    answer[i] = i - lastIndex
                 }
                 charIndexMap[char] = i
             }
-            
             return answer
         }
     }
