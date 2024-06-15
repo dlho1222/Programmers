ee8e142345aede829e1cdf25dc6762bcfccde71d
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-15 13:31:50 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 26.91 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.kt
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.kt
@@ -1,17 +1,5 @@
 class Solution {
-        fun solution(numbers: IntArray): IntArray {
-            var answer: IntArray = intArrayOf()
-            var setList = hashSetOf<Int>()
-
-            for (i in 0 until numbers.size) {
-                for (j in 0 until numbers.size) {
-                    if(i != j){
-                        setList += numbers[i] + numbers[j]
-                    }
-                }
-            }
-            answer += setList.sorted()
-            
-            return answer
-        }
+        fun solution(numbers: IntArray): IntArray =
+            numbers.indices.flatMap { j -> (j + 1 until numbers.size).map { i -> numbers[i] + numbers[j] } }
+                .toSet().sorted().toIntArray()
     }
