976c4512a24be8ba68ad8be6eb3dd1925cc25130
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-18 12:41:19 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 22.40 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
@@ -1,9 +1,4 @@
 class Solution {
-        fun solution(strings: Array<String>, n: Int): Array<String> {
-            var answer = arrayOf<String>()
-            answer = strings
-            answer.toList().sorted()
-           
-            return answer.sorted().sortedBy { it[n] }.toList().toTypedArray()
-        }
+        fun solution(strings: Array<String>, n: Int): Array<String> =
+            strings.sorted().sortedBy { it[n] }.toTypedArray()
     }
