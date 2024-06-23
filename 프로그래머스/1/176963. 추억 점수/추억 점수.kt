50bd6048cdb65de25e8978c36b3decb0babc6861
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-06-23 11:09:16 +0900

    [level 1] Title: 추억 점수, Time: 19.16 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.kt
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.kt
@@ -3,15 +3,11 @@ class Solution {
             name: Array<String>,
             yearning: IntArray,
             photo: Array<Array<String>>
-        ): IntArray {
-            var answer: IntArray = intArrayOf()
-            val scores = mutableMapOf<String, Int>()
-            name.mapIndexed { index, s -> scores.put(s, yearning[index]) }
-            answer = photo.map { row ->
-                row.sumOf {
-                    if (scores.containsKey(it)) scores[it]!!.toInt() else 0
-                }
-            }.toIntArray()
-            return answer
-        }
+        ): IntArray = photo.map { row ->
+            row.sumOf {
+                if (name.indexOf(it) > -1) yearning[name.indexOf(
+                    it
+                )] else 0
+            }
+        }.toIntArray()
     }
