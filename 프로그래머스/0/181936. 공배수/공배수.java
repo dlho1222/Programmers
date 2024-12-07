7739d4d69509e2eb159cfcc617768dc380fc7c13
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-12-07 15:58:04 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        if (n != 0 && m != 0) {
-            if (number % n == 0 && number % m == 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }else{
-            return 0; 
-        }
+        return number % n == 0 && number % m == 0 ? 1 : 0;
     }
 }
