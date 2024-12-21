27fb393a8f019fceeaed5baafa8ed1d3233495ea
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-12-21 19:30:41 +0900

    [level 2] Title: 튜플, Time: 103.63 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.kt
+++ 프로그래머스/2/64065. 튜플/튜플.kt
@@ -1,11 +1,7 @@
 class Solution {
-        fun solution(s: String): IntArray {
-            val sortedList = s.substring(2, s.length - 2).split("},{")
-                .map { it.split(",").map { it.toInt() }.toSet() }.sortedBy { it.size }
-            val result = mutableListOf<Int>()
-            for (set in sortedList) {
-                result.addAll(set - result.toSet())
-            }
-            return result.toIntArray()
-        }
+        fun solution(s: String): IntArray =
+            s.substring(2, s.length - 2).split("},{")
+                .map { it.split(",").map { it.toInt() }.toSet() }.sortedBy { it.count() }.fold(
+                    listOf<Int>()
+                ) { acc, set -> acc + (set - acc) }.toIntArray()
     }
