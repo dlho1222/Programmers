b947b809c511595208695f73049690493e31dd7d
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-12-25 13:15:35 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.16 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
@@ -1,5 +1,20 @@
 class Solution {
-    fun solution(s: String): String =
-        s.split(" ").map { it.lowercase() }
-                .joinToString(" ") { it.replaceFirstChar { char -> if (char.isLetter()) char.uppercaseChar() else char.lowercaseChar() } }
-}
+        fun solution(s: String): String {
+            val sb: StringBuilder = StringBuilder("")
+            var isFirst = true
+            for (c in s) {
+                if (c == ' ') {
+                    sb.append(c)
+                    isFirst = true
+                } else {
+                    if (isFirst) {
+                        sb.append(c.uppercaseChar())
+                        isFirst = false
+                    } else {
+                        sb.append(c.lowercaseChar())
+                    }
+                }
+            }
+            return sb.toString()
+        }
+    }
