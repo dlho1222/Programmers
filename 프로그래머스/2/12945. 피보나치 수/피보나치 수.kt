677f395ba8f3ecf95fd729e7926b7d40db573287
Dlho <127379797+dlho1222@users.noreply.github.com>
2024-12-28 14:12:17 +0900

    [level 2] Title: 피보나치 수, Time: 6.36 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.kt
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.kt
@@ -1,7 +1,7 @@
 class Solution {
-        fun solution(n: Int): Int = mutableListOf(0, 1).apply {
+        fun solution(n: Int): Int = Array(n + 1) { if (it < 2) it else 0 }.apply {
             for (i in 2..n) {
-                add((this[i - 1] + this[i - 2]) % 1234567)
+                this[i] = (this[i - 1] + this[i - 2]) % 1234567
             }
-        }.last()
+        }[n]
     }
