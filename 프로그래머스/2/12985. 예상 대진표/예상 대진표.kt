86030b927f7e137ab5b28e85f435225159f75fc6
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-01-07 13:55:16 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
@@ -1,8 +1,8 @@
 class Solution {
         fun solution(n: Int, a: Int, b: Int): Int {
-            var round = 0
             var playerA = a
             var playerB = b
+            var round = 0
             while (playerA != playerB) {
                 playerA = (playerA + 1) / 2
                 playerB = (playerB + 1) / 2
