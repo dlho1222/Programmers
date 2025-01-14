e9b965828c4b9f2ef53d815e119ea763417b427c
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-01-14 21:00:17 +0900

    [level 1] Title: 대충 만든 자판, Time: 14.29 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
@@ -2,26 +2,19 @@ class Solution {
         fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
             var answer: IntArray = intArrayOf()
             val keyboard = mutableMapOf<Char, Int>()
-            for ((index, key) in keymap.withIndex()) {
-                key.forEachIndexed { pressed, char ->
-                    val currentPressed = keyboard[char] ?: Int.MAX_VALUE
-                    keyboard[char] = minOf(currentPressed, pressed + 1)
+            keymap.forEach { key ->
+                key.forEachIndexed { index, char ->
+                    keyboard[char] = minOf(keyboard[char] ?: Int.MAX_VALUE, index + 1)
                 }
             }
 
-            for (target in targets) {
+            return targets.map { target ->
                 var totalPressed = 0
                 for (char in target) {
-                    var pressed = keyboard[char]
-                    if (pressed == null){
-                        totalPressed = -1
-                        break
-                    }else{
-                        totalPressed += pressed
-                    }
+                    val pressed = keyboard[char] ?: return@map -1
+                    totalPressed += pressed
                 }
-                answer += totalPressed
-            }
-            return answer
+                totalPressed
+            }.toIntArray()
         }
     }
