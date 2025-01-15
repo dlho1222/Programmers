e71565d501385f0a14de15f2b77b982b3760b731
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-01-15 13:58:20 +0900

    [level 1] Title: 신규 아이디 추천, Time: 48.53 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.kt
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.kt
@@ -1,20 +1,7 @@
 class Solution {
-        fun solution(new_id: String): String {
-            val sb = StringBuilder("")
-            var lastChar: Char? = null
-
-            for (char in new_id.lowercase()) {
-                if (char in 'a'..'z' || char in '0'..'9' || char == '-' || char == '_' || char == '.') {
-                    if (char == '.' && lastChar == '.') continue
-                    sb.append(char)
-                    lastChar = char
+        fun solution(new_id: String): String =
+            new_id.lowercase().replace("[^a-z0-9-_.]".toRegex(), "").replace("[.]+".toRegex(), ".")
+                .trim('.').let { it.ifEmpty { "a" } }.take(15).trimEnd('.').let {
+                    it.padEnd(3, it.last())
                 }
-            }
-
-            var result = sb.toString().trim('.')
-            if (result.isEmpty()) result = "a"
-            if (result.length >= 16) result = result.substring(0, 15).trimEnd('.')
-            while (result.length < 3) result += result.last()
-            return result
-        }
     }
