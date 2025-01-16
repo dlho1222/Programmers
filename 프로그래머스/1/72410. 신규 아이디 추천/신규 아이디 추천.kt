1a6bf40307e4ef02d70fd46e56f2afa42b8094b8
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-01-16 10:35:13 +0900

    [level 1] Title: 신규 아이디 추천, Time: 41.07 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.kt
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.kt
@@ -1,7 +1,7 @@
 class Solution {
         fun solution(new_id: String): String =
             new_id.lowercase().replace("[^a-z0-9-_.]".toRegex(), "").replace("[.]+".toRegex(), ".")
-                .trim('.').let { it.ifEmpty { "a" } }.take(15).trimEnd('.').let {
-                    it.padEnd(3, it.last())
-                }
+                .trim('.').let {
+                it.ifEmpty { "a" }.take(15).trimEnd('.').let { it.padEnd(3, it.last()) }
+            }
     }
