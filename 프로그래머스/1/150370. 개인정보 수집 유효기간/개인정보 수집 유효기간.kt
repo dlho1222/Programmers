149a3860bd7fd3bfad12c0f9c9b2d6a77b07e6fd
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-01-28 20:48:04 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 24.54 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
@@ -1,22 +1,20 @@
 class Solution {
         fun solution(today: String, terms: Array<String>, privacies: Array<String>): IntArray {
-            var answer: IntArray = intArrayOf()
-            val (year, month, day) = today.split(".").map { it.toInt() }
-            val todayTotal = (year * 12 * 28) + (month * 28) + day
-            val termMap = terms.associate { it.split(" ")[0] to it.split(" ")[1] }
+            val (todayYear, todayMonth, todayDay) = today.split(".").map { it.toInt() }
+            val todayTotal = (todayYear * 12 * 28) + (todayMonth * 28) + todayDay
+            val termMap = terms.associate {
+                val (type, duration) = it.split(" ")
+                type to duration.toInt() * 28
+            }
 
-            privacies.forEachIndexed { index, privacy ->
+            return privacies.mapIndexedNotNull { index, privacy ->
                 val (date, type) = privacy.split(" ")
                 val (year, month, day) = date.split(".").map { it.toInt() }
+                val privacyTotal = (year * 12 * 28) + (month * 28) + day
 
-                val privacyTotal =
-                    (year * 12 * 28) + (month * 28) + day
-
-                val expirationTotal = privacyTotal + (termMap[type]?.toInt() ?: 0) * 28
-                if (todayTotal >= expirationTotal) {
-                    answer += index + 1
-                }
-            }
-            return answer
+                if (todayTotal >= privacyTotal + (termMap[type] ?: 0)) {
+                    index + 1
+                } else null
+            }.toIntArray()
         }
     }
