23e69417426c0887c83f2a56c859f5ee196483dd
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-01-29 11:36:12 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.18 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.kt
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.kt
@@ -1,21 +1,21 @@
 class Solution {
         fun solution(wallpaper: Array<String>): IntArray {
-            var answer: IntArray = intArrayOf()
-            var minRow = Int.MAX_VALUE
             var minCol = Int.MAX_VALUE
-            var maxRow = Int.MIN_VALUE
+            var minRow = Int.MAX_VALUE
             var maxCol = Int.MIN_VALUE
+            var maxRow = Int.MIN_VALUE
 
             for (i in wallpaper.indices) {
                 for (j in wallpaper[i].indices) {
                     if (wallpaper[i][j] == '#') {
-                        minRow = minOf(minRow, i)
-                        minCol = minOf(minCol, j)
-                        maxRow = maxOf(maxRow, i)
-                        maxCol = maxOf(maxCol, j)
+                        minCol = minOf(minCol, i)
+                        minRow = minOf(minRow, j)
+                        maxCol = maxOf(maxCol, i)
+                        maxRow = maxOf(maxRow, j)
                     }
                 }
             }
-            return intArrayOf(minRow, minCol, maxRow + 1, maxCol + 1)
+
+            return intArrayOf(minCol, minRow, maxCol + 1, maxRow + 1)
         }
     }
