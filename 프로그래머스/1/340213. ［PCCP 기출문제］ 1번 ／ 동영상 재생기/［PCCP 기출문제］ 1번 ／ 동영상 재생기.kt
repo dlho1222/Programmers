fc8ae7eb9dfe8d38779d44286ad1ea84c61bd5c0
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-02-02 20:26:56 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 22.26 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.kt
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.kt
@@ -6,38 +6,38 @@ class Solution {
             op_end: String,
             commands: Array<String>
         ): String {
-            val videoLength = video_len.split(":").map { it.toInt() }.let { (it[0] * 60) + it[1] }
-            val position = pos.split(":").map { it.toInt() }.let { (it[0] * 60) + it[1] }
-            val openingStart = op_start.split(":").map { it.toInt() }.let { (it[0] * 60) + it[1] }
-            val openingEnd = op_end.split(":").map { it.toInt() }.let { (it[0] * 60) + it[1] }
-            val prev = -10
-            val next = 10
+            val videoLength = toSeconds(video_len)
+            val position = toSeconds(pos)
+            val openingStart = toSeconds(op_start)
+            val openingEnd = toSeconds(op_end)
             var currentPosition = if (position in openingStart..openingEnd) openingEnd else position
+
             for (command in commands) {
                 when (command) {
                     "next" -> {
-                        currentPosition += next
-                        if (currentPosition > videoLength) {
-                            currentPosition = videoLength
-                        } else if (currentPosition in openingStart..openingEnd) {
-                            currentPosition = openingEnd
-                        }
+                        if (videoLength - currentPosition < 10) currentPosition =
+                            videoLength else currentPosition += 10
                     }
 
                     "prev" -> {
-                        currentPosition += prev
-                        if (currentPosition < 0) {
-                            currentPosition = 0
-                        } else if (currentPosition in openingStart..openingEnd) {
-                            currentPosition = openingEnd
-                        }
+                        if (currentPosition - 10 < 0) currentPosition = 0 else currentPosition -= 10
                     }
                 }
                 if (currentPosition in openingStart..openingEnd) {
                     currentPosition = openingEnd
                 }
             }
+            return toTimeString(currentPosition)
+        }
 
-            return "%02d:%02d".format(currentPosition / 60, currentPosition % 60)
+        fun toSeconds(time: String): Int {
+            val (minute, seconds) = time.split(":").map { it.toInt() }
+            return (minute * 60 + seconds)
+        }
+
+        fun toTimeString(currentPosition: Int): String {
+            val minute = currentPosition / 60
+            val seconds = currentPosition % 60
+            return "%02d:%02d".format(minute, seconds)
         }
     }
