3e07deec7662121d81533c890f3c1e0e11ad6210
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-02-20 20:01:26 +0900

    [level 2] Title: 프로세스, Time: 22.65 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.kt
+++ 프로그래머스/2/42587. 프로세스/프로세스.kt
@@ -1,15 +1,17 @@
 class Solution {
         fun solution(priorities: IntArray, location: Int): Int {
+            data class Priority(val priority: Int, val index: Int)
+
+            val deque = ArrayDeque<Priority>()
+            priorities.forEachIndexed { idx, priority -> deque.addLast(Priority(priority, idx)) }
             var answer = 0
-            val deque = ArrayDeque<Pair<Int, Int>>()
-            priorities.forEachIndexed { index, value -> deque.addLast(Pair(value, index)) }
-            while (true) {
+            while (deque.isNotEmpty()) {
                 val element = deque.removeFirst()
-                if (deque.any { it.first > element.first }) {
+                if (deque.any { it.priority > element.priority }) {
                     deque.addLast(element)
                 } else {
                     answer++
-                    if (element.second == location) break
+                    if (element.index == location) return answer
                 }
             }
             return answer
