7314981ceb5d71d41ce93af0edd7dc6a3e7a85c6
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-05-26 21:33:47 +0900

    [level 0] Title: 배열의 평균값, Time: 0.00 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,6 +1,1 @@
-def solution(numbers):
-    answer = 0
-    for number in numbers:
-        answer += number
-    answer = answer / len(numbers)
-    return answer
+solution = lambda numbers : sum(numbers) / len(numbers)
