e13f9fbd0185fba7bc5a32c1e4e3298d6d65b2d6
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-05-26 21:10:16 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,6 +1,1 @@
-def solution(n, k):
-    if n < 10:
-        return (n * 12000) + (k * 2000)
-    else:
-        return (n * 12000) + (k * 2000) - (n // 10) * 2000
-    return answer
+solution = lambda n, x: (n * 12000) + (x * 2000) - ((n // 10) * 2000)
