b969f2775fb852c8347aeeb8a31cd96bfefcd780
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-05-28 22:38:39 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,6 +1,2 @@
 def solution(n, numlist):
-    answer = []
-    for i in numlist:
-        if i % n == 0:
-            answer.append(i)
-    return answer
+    return list(filter(lambda x: x % n == 0, numlist))
