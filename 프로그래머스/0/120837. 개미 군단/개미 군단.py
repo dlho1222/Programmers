a3a9689b9cf121f16ed756614a3b5af04e43d384
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-05-29 20:11:56 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,17 +1,2 @@
 def solution(hp):
-    answer = 0
-    while hp > 0:
-        if hp > 5:
-            div = divmod(hp, 5)
-            answer = div[0]
-            hp = div[1]
-        elif hp >= 3:
-            div = divmod(hp, 3)
-            answer += div[0]
-            hp = div[1]
-        else:
-            div = divmod(hp, 1)
-            answer += div[0]
-            hp = div[1]
-
-    return answer
+    return hp // 5 + (hp % 5 // 3) + ((hp % 5) % 3)
