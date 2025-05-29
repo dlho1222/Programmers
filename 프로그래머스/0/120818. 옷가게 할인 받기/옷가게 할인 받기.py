ba9117df67c068f04dfb28a11da9cd4d02133e4d
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-05-29 19:31:25 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,9 +1,9 @@
 def solution(price):
     if price >= 500000:
         return int(price * 0.8)
-    elif price < 500000 and price >= 300000:
+    elif price >= 300000:  
         return int(price * 0.9)
-    elif price < 300000 and price >= 100000:
+    elif price >= 100000: 
         return int(price * 0.95)
     else:
         return price
