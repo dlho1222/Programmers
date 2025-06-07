13c56d236d4ec37afbb4ec734a965b9f97603626
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-06-07 21:19:10 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,9 +1,5 @@
 def solution(num_list):
-    answer = 0
-    for i in num_list:
-        if i < 0 :
-            answer = num_list.index(i)
-            break 
-        else :
-            answer = -1 
-    return answer
+    for i in range(len(num_list)):
+        if num_list[i] < 0:
+            return i
+    return -1
