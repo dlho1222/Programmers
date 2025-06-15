fbc525d77d3ecf3c971dc788d8391d9233f64948
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-06-15 21:23:43 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    endLen = len(my_string)
-    answer = []
-    for i in range(1, endLen):
-        answer.append(my_string[i:endLen])
-   
-    answer.append(my_string)
-    return sorted(answer)
+    return sorted([my_string[i:] for i in range(len(my_string))])
